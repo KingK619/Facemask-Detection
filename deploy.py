261cc5c71837ef83c01b607cb967b7d94cd18aa0
KV <kv@pop-os.localdomain>
2021-06-05 19:18:49 +0530

    changes

--- deploy.py
+++ deploy.py
@@ -1,19 +1,17 @@
 
-import streamlit as st
-from streamlit_webrtc import VideoProcessorBase, webrtc_streamer,ClientSettings
-import cv2
-from PIL import Image,ImageEnhance
-import numpy as np 
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
-from imutils import paths
-import numpy as np
 import cv2
 import imghdr
 import os
+import numpy as np 
+import streamlit as st
+from imutils import paths
 from pathlib import Path
+import matplotlib.pyplot as plt
+from PIL import Image,ImageEnhance
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, ClientSettings
 st.set_page_config(layout="wide")
 
 
@@ -44,64 +42,66 @@ st.sidebar.title("Explore the Following")
 
 # --------------------------- Functions ----------------------------------
 
-def detect_and_predict_mask(frame,net,model):
-    #grab the dimensions of the frame and then construct a blob
-    (h,w)=frame.shape[:2]
-    blob=cv2.dnn.blobFromImage(frame,1.0,(300,300),(104.0,177.0,123.0))
-    
-    net.setInput(blob)
-    detections=net.forward()
-    
-    #initialize our list of faces, their corresponding locations and list of predictions
-    
-    faces=[]
-    locs=[]
-    preds=[]
-    
-    
-    for i in range(0,detections.shape[2]):
-        confidence=detections[0,0,i,2]
-    
-    
-        if confidence>0.5:
-        #we need the X,Y coordinates
-            box=detections[0,0,i,3:7]*np.array([w,h,w,h])
-            (startX,startY,endX,endY)=box.astype('int')
-        
-            #ensure the bounding boxes fall within the dimensions of the frame
-            (startX,startY)=(max(0,startX),max(0,startY))
-            (endX,endY)=(min(w-1,endX), min(h-1,endY))
-        
-            #extract the face ROI, convert it from BGR to RGB channel, resize it to 224,224 and preprocess it
-            face=frame[startY:endY, startX:endX]
-            face=cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
-            face=cv2.resize(face,(96,96))
-            face=img_to_array(face)
-            face=preprocess_input(face)
-        
-            faces.append(face)
-            locs.append((startX,startY,endX,endY))
-        
-        #only make a predictions if atleast one face was detected
-    if len(faces)>0:
-        faces=np.array(faces,dtype='float32')
-        preds=model.predict(faces,batch_size=12)
-        
-    return (locs,preds)
+class VideoTransformer(VideoTransformerBase):
+    def detect_and_predict_mask(self,frame1,net,model):
+        #grab the dimensions of the frame and then construct a blob
+        (h,w)=frame1.shape[:2]
+        blob=cv2.dnn.blobFromImage(frame1,1.0,(300,300),(104.0,177.0,123.0))
 
-class VideoTransformer(VideoProcessorBase):
-    def recv(self, frame):
-        st.image(frame)
-        frame=imutils.resize(frame,width=400)
-        (locs,preds)=detect_and_predict_mask(frame,net,model)
+        net.setInput(blob)
+        detections=net.forward()
+
+        #initialize our list of faces, their corresponding locations and list of predictions
+
+        faces=[]
+        locs=[]
+        preds=[]
+
+
+        for i in range(0,detections.shape[2]):
+            confidence=detections[0,0,i,2]
+
+
+            if confidence>0.7:
+            #we need the X,Y coordinates
+                box=detections[0,0,i,3:7]*np.array([w,h,w,h])
+                (startX,startY,endX,endY)=box.astype('int')
+
+                #ensure the bounding boxes fall within the dimensions of the frame
+                (startX,startY)=(max(0,startX),max(0,startY))
+                (endX,endY)=(min(w-1,endX), min(h-1,endY))
+
+                #extract the face ROI, convert it from BGR to RGB channel, resize it to 224,224 and preprocess it
+                face=frame1[startY:endY, startX:endX]
+                face=cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
+                face=cv2.resize(face,(96,96))
+                face=img_to_array(face)
+                face=preprocess_input(face)
+
+                faces.append(face)
+                locs.append((startX,startY,endX,endY))
+
+            #only make a predictions if atleast one face was detected
+        if len(faces)>=1:
+            faces=np.array(faces,dtype='float32')
+            preds=model.predict(faces,batch_size=12)
+
+        return (locs,preds)
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        net=cv2.dnn.readNet(weightsPath,prototxtPath)
+        model=load_model(r'model/custom_4000_32_100.h5')
+        (locs,preds)=self.detect_and_predict_mask(img,net,model)
         for (box,pred) in zip(locs,preds):
             (startX,startY,endX,endY)=box
             (mask,withoutMask)=pred
             label='Mask' if mask>withoutMask else 'No Mask'
             color=(0,255,0) if label=='Mask' else (0,0,255)
-            cv2.putText(frame,label,(startX,startY-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
-            cv2.rectangle(frame,(startX,startY),(endX,endY),color,2)
-        return frame
+            #include the probability in the label
+            label="{}: {:.2f}%".format(label,max(mask,withoutMask)*100)
+            img=cv2.putText(img,label,(startX,startY-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
+            img=cv2.rectangle(img,(startX,startY),(endX,endY),color,2)
+        return img
 
 
 
@@ -165,15 +165,15 @@ app_mode = st.sidebar.selectbox(
 
 
 if app_mode == SIDEBAR_OPTION_WEBCAM:
-    st.subheader("Coming Soon!")
-    ctx = webrtc_streamer(
-        client_settings=ClientSettings(rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-                                       media_stream_constraints={"video": True, "audio": False},
-        ),
-        video_transformer_factory=VideoTransformer,
-        key="neural-style-transfer",
-    )
-    
+    st.text('In Develpment')
+    ctx=webrtc_streamer(client_settings=ClientSettings(
+                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+                media_stream_constraints={"video": True, "audio": False},
+                ),
+                key="face-mask-detection", 
+                video_transformer_factory=VideoTransformer
+               )
+   
 
 elif app_mode == SIDEBAR_OPTION_UPLOAD_IMAGE:
     image_file = st.file_uploader("Upload Image", type=['png', 'jpeg', 'jpg'])
@@ -263,21 +263,20 @@ elif app_mode == SIDEBAR_OPTION_MEET_TEAM:
     st.write("------")
     st.sidebar.write('Please feel free to connect with us on Linkedin!')
     st.sidebar.success('Hope you had a great time :)')
-    col1,col2,col3 = st.beta_columns([3,3,3])
+    col1,col2,col3 = st.beta_columns([2,2,2])
     with col1:
-	    st.image(os.path.join(DEFAULT_DATA_BASE_DIR,TEAM_DIR,'kv.jpeg'),caption="Karanveer Sidana")
+	    st.image(os.path.join(DEFAULT_DATA_BASE_DIR,TEAM_DIR,'kv.jpeg'),caption="Karanveer Sidana",use_column_width=True)
     with col2:
-        st.image(os.path.join(DEFAULT_DATA_BASE_DIR,TEAM_DIR,'h2.jpeg'),caption="Hiten Goyal")
+        st.image(os.path.join(DEFAULT_DATA_BASE_DIR,TEAM_DIR,'h2.jpeg'),caption="Hiten Goyal",use_column_width=True)
     with col3:
-	    st.image(os.path.join(DEFAULT_DATA_BASE_DIR,TEAM_DIR,'channa.png'),caption="Charanjeet Singh")
+	    st.image(os.path.join(DEFAULT_DATA_BASE_DIR,TEAM_DIR,'channa.png'),caption="Charanjeet Singh",use_column_width=True)
 
     expandar_linkedin = st.beta_expander('Contact Information')
     expandar_linkedin.write(
         'Karan: https://www.linkedin.com/in/karanveer-sidana-07a49b1b1/')
     expandar_linkedin.write('Hiten: https://www.linkedin.com/in/hitengoyal/')
-    expandar_linkedin.write('Charanjeet: https://www.linkedin.com/')
     expander_faq = st.beta_expander("More About Our Project")
-    expander_faq.write("Hi there! If you have any questions about our project, or simply want to check out the source code, please visit our github repo: https://github.com/robertklee/COCO-Human-Pose")
+    expander_faq.write("Hi there! If you have any questions about our project, or simply want to check out the source code, please visit our github repo: https://github.com/KingK619/Facemask-Detection")
 
 else:
 	raise ValueError(
